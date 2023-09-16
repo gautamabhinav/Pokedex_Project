f5058b42cb67078eb804014325319d0356505e3f
gautamabhinav <abhinavrajgautam01gmail.com>
2023-09-16 12:05:52 +0530

    Added Debouncing in Pokedex

--- src/hooks/usePokemonDetails.js
+++ src/hooks/usePokemonDetails.js
@@ -1,40 +1,48 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import usePokemonList from "./usePokemonList";
 
-function usePokemonDetails(id) {
+function usePokemonDetails(id, pokemonName) {
     const [pokemon, setPokemon] = useState({});
-    let pokemonListHookResponse = [];
     async function downloadPokemon() {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        console.log(response.data);
-        const pokemonOfSameTypes = axios.get(`https://pokeapi.co/api/v2/type/${response.data.types ? response.data.types[0].type.name : ''}`);
-        console.log('s', pokemonOfSameTypes)
-        setPokemon(state => ({
-            ...state,
-            name: response.data.name,
-            image: response.data.sprites.other.dream_world.front_default,
-            weight: response.data.weight,
-            height: response.data.height,
-            types: response.data.types.map((t) => t.type.name)
-        }));
-
-        pokemonOfSameTypes.then((response) => {
+        try {
+            let response;
+            if(pokemonName){
+                console.log('fetching by name')
+                response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            } else {
+                response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            }
+            console.log(response.data);
+            const pokemonOfSameTypes = axios.get(`https://pokeapi.co/api/v2/type/${response.data.types ? response.data.types[0].type.name : ''}`);
+            console.log('s', pokemonOfSameTypes)
             setPokemon(state => ({
                 ...state,
-                similarPokemons:  response.data.pokemon
+                name: response.data.name,
+                image: response.data.sprites.other.dream_world.front_default,
+                weight: response.data.weight,
+                height: response.data.height,
+                types: response.data.types.map((t) => t.type.name)
             }));
-        })
-        console.log(response.data.types);
-        setPokemonListState({... pokemonListState, type: response.data.types ? response.data.types[0].type.name : '' })
+
+            pokemonOfSameTypes.then((response) => {
+                setPokemon(state => ({
+                    ...state,
+                    similarPokemons:  response.data.pokemon
+                }));
+            })
+            console.log(response.data.types);
+            setPokemonListState({... pokemonListState, type: response.data.types ? response.data.types[0].type.name : '' })
+        } catch (error) {
+            console.log('Something went wrong');
+        }
+        
     }
 
-    const [pokemonListState, setPokemonListState] = usePokemonList(); 
+    const [pokemonListState, setPokemonListState] = useState({}); 
 
 
     useEffect(() => {
-        downloadPokemon();
-        console.log('list',pokemonListHookResponse.pokemonListState);
+            downloadPokemon();
     }, []);
 
     return [pokemon];
